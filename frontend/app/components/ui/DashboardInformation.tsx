70f778f4073d097c2703781e7217c03907bc8fbf
Sean Bryan <162139885+CadeCodes19@users.noreply.github.com>
2026-03-30 01:02:31 +0800

    mock version of the dashboard information ui

---
+++ frontend/app/components/ui/DashboardInformation.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { type LucideIcon } from "lucide-react";
+
+export interface DashboardInformationProps {
+  label: string;
+  labelValue: number;
+  labelValueColor: string;
+  icon: LucideIcon;
+  iconColor: string;
+  iconBackground: string;
+}
+const DashboardInformation = ({
+  label,
+  labelValue,
+  labelValueColor,
+  icon: Icon,
+  iconBackground,
+  iconColor,
+}: DashboardInformationProps) => {
+  return (
+    <div className="bg-white shadow-md rounded-md p-4 flex items-center justify-between">
+      <div className="flex flex-col gap-2">
+        <p className="text-xs md:text-sm text-gray-500">{label}</p>
+        <h1 className={`${labelValueColor} text-lg md:text-xl`}>
+          $ {labelValue}.00
+        </h1>
+      </div>
+
+      <div className={`rounded-full ${iconBackground} w-fit p-2`}>
+        <Icon className={`${iconColor}`} />
+      </div>
+    </div>
+  );
+};
+
+export default DashboardInformation;
--- frontend/app/routes/Dashboard.tsx
+++ frontend/app/routes/Dashboard.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 import type { Route } from "./+types/Dashboard";
+import { Wallet, TrendingUp, TrendingDown } from "lucide-react";
+import type { DashboardInformationProps } from "~/components/ui/DashboardInformation";
+import DashboardInformation from "~/components/ui/DashboardInformation";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -17,10 +20,55 @@ export function meta({}: Route.MetaArgs) {
     },
   ];
 }
+
+const dashboardColumns: DashboardInformationProps[] = [
+  {
+    label: "Total Balance",
+    labelValue: 19,
+    labelValueColor: "text-green-600",
+    icon: Wallet,
+    iconBackground: "bg-indigo-200",
+    iconColor: "text-indigo-600",
+  },
+  {
+    label: "Total Income",
+    labelValue: 90,
+    labelValueColor: "text-green-600",
+    icon: TrendingUp,
+    iconBackground: "bg-green-200",
+    iconColor: "text-green-600",
+  },
+  {
+    label: "Total Expenses",
+    labelValue: 71,
+    labelValueColor: "text-red-400",
+    icon: TrendingDown,
+    iconBackground: "bg-red-200",
+    iconColor: "text-red-600",
+  },
+];
+
 const Dashboard = () => {
   return (
-    <section className="max-w-6xl mx-auto mt-4">
-      <div className="px-4">Dashboard</div>
+    // Background
+    <section className="min-h-screen bg-gray-50 py-8">
+      {/*Content Container */}
+      <div className="max-w-6xl mx-auto px-4">
+        {/* Column Heading Container*/}
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+          {/* Column Headings */}
+          {dashboardColumns.map((col) => (
+            <DashboardInformation
+              label={col.label}
+              labelValue={col.labelValue}
+              labelValueColor={col.labelValueColor}
+              icon={col.icon}
+              iconBackground={col.iconBackground}
+              iconColor={col.iconColor}
+            />
+          ))}
+        </div>
+      </div>
     </section>
   );
 };
