6887e756ca7e4356cd4054b5fc89998e9f53897e
Sean Bryan <seanbryan.noces@gmail.com>
2026-04-25 23:59:33 +0800

    fixes

--- backend/server.js
+++ backend/server.js
@@ -17,11 +17,11 @@ app.use(cors());
 const { Pool } = pkg;
 
 export const pool = new Pool({
-  user: process.env.DB_USER || "postgres",
-  host: process.env.DB_HOST || "localhost",
-  database: process.env.DB_NAME || "finance_tracker",
-  password: process.env.DB_PASSWORD || "101906",
-  port: process.env.DB_PORT || 5432,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
 });
 
 app.listen(port, () => {
--- frontend/app/services/transactionService.ts
+++ frontend/app/services/transactionService.ts
@@ -1,6 +1,6 @@
 import { apiFetch } from "~/utils/api/apiFetch";
 
-const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:5000/api/v1";
+const BASE_URL = import.meta.env.VITE_API_URL;
 
 type dataProps = {
   category: number;
